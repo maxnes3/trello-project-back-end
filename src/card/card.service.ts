65cd2c569e61a3578a4f366e44419e4682b09b40
maxnes3 <draw_max@mail.ru>
2024-08-29 00:42:39 +0400

    update: card

--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { InsertCardDto, UpdateCardDto } from './dto/card.dto';
 
@@ -19,12 +19,59 @@ export class CardService {
         });
     }
 
-    insert(dto: InsertCardDto) {
-        
+    async insert(dto: InsertCardDto) {
+        const maxPosition = await this.prismaService.card.aggregate({
+            _max: {
+              position: true,
+            },
+            where: {
+              columnId: dto.columnId,
+            },
+        });
+
+        const newPosition = maxPosition._max.position ? maxPosition._max.position + 1 : 1;
+
+        return this.prismaService.card.create({
+            data: {
+                columnId: dto.columnId,
+                title: dto.title.trim(),
+                position: newPosition,
+            },
+        });
     }
 
-    update(dto: UpdateCardDto) {
+    async update(dto: UpdateCardDto) {
+        const card = await this.prismaService.card.findUnique({
+            where: { id: dto.id },
+        });
+      
+        if (!card) {
+            throw new NotFoundException('Card not found');
+        }
 
+        if (dto.position) {
+            await this.prismaService.card.updateMany({
+                where: {
+                    columnId: card.columnId,
+                    position: {
+                        gte: dto.position,
+                    },
+                },
+                data: {
+                    position: {
+                        increment: 1,
+                    },
+                },
+            });
+        }
+
+        return this.prismaService.card.update({
+            where: { id: dto.id },
+            data: {
+              title: dto.title ?? card.title,
+              position: dto.position ?? card.position,
+            },
+        });
     }
     
     delete(cardId: string) {
