84cee503f1c636b46d4b4a54d0e8afd2f6490f08
maxnes3 <draw_max@mail.ru>
2024-08-29 21:40:25 +0400

    update: add description

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Trello-Project')
-    .setDescription('The project used: Nest.js, PostgreSQL, JWT Authorization, REST API, Validation pipe, Guards and Swagger.')
+    .setDescription('The project used: Nest.js, PostgreSQL, JWT Authorization, REST API, Validation pipe, Guards and Swagger. ER Diagram: https://dbdiagram.io/d/TrelloProject_ER_Diagram-66cb79b63f611e76e96d1b2b')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
